95a68990e59ca040cb79b532b308bf844477ed62
javigal4 <javigalbal@gmail.com>
2018-05-18 21:11:00 +0200

    Cambios

--- src/main/java/com/jgb/formacionATSistemas/model/Course.java
+++ src/main/java/com/jgb/formacionATSistemas/model/Course.java
@@ -7,8 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import lombok.Getter;
