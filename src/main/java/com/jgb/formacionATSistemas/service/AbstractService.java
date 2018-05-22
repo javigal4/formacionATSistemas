eb0d4a24a37b3cfe4a56e474fb2418fd8454554f
javigal4 <javigalbal@gmail.com>
2018-05-22 16:51:10 +0200

    Service, ServiceImp y DAO abstracto

---
+++ src/main/java/com/jgb/formacionATSistemas/service/AbstractService.java
@@ -0,0 +1,20 @@
+package com.jgb.formacionATSistemas.service;
+
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.data.domain.Pageable;
+
+public interface AbstractService<T, ID> extends Serializable {
+
+	T create(T t);
+	
+	void update(T t);
+	
+	Optional<T> findById(ID id);
+	
+	Set<T> findAll(Pageable p);
+	
+	void delete(T t);
+}
---
+++ src/main/java/com/jgb/formacionATSistemas/service/AbstractServiceImp.java
@@ -0,0 +1,46 @@
+package com.jgb.formacionATSistemas.service;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.jgb.formacionATSistemas.dao.AbstractDAO;
+import com.jgb.formacionATSistemas.dao.UserDAO;
+import com.jgb.formacionATSistemas.model.User;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class AbstractServiceImp<T> implements AbstractService<T, Integer>{
+	
+	@Autowired
+	AbstractDAO<T> dao;
+	
+	@Override
+	public T create(T t) {
+		return dao.save(t);
+	}
+	
+	@Override
+	public void update(T t) {
+		dao.save(t);
+	}
+	
+	@Override
+	public Optional<T> findById(Integer id) {
+		return dao.findById(id);
+	}
+	
+	@Override
+	public Set<T> findAll(Pageable p) {
+		return dao.findAll(PageRequest.of(p.getPageNumber(), p.getPageSize())).stream().collect(Collectors.toSet());
+	}
+	
+	@Override
+	public void delete(T t) {
+		dao.delete(t);
+	}
+}
