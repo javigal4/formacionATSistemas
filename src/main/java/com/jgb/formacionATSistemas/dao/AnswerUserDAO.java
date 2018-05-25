755cecfb8985a370c1f7b7e4e94dfc3fce0077d4
javigal4 <javigalbal@gmail.com>
2018-05-25 17:17:44 +0200

    Corrección de errores en DAO

---
+++ src/main/java/com/jgb/formacionATSistemas/dao/AnswerUserDAO.java
@@ -0,0 +1,13 @@
+package com.jgb.formacionATSistemas.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jgb.formacionATSistemas.model.AnswerUser;
+
+@Repository
+public interface AnswerUserDAO extends PagingAndSortingRepository<AnswerUser, Integer>{
+
+}
